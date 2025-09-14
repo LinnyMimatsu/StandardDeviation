2cd97ad44da9da631cf6d49b4359043f763dacc7
linn <dapressler@gmail.com>
2025-09-13 23:32:19 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ * Name: Dom
+ * Description: Read in a list of floating point numbers
+ * of user-specified N
+ * Calculate the standard deviation of the numbers.
+ * Date: 09/13/25
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+int main(void) {
+
+    return 0;
+}
